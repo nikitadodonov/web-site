405730d72d32053c848716f93070425b30bae290
nikitadodonov <nikita.dodonoff@yandex.ru>
2016-11-05 19:43:51 +0300

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -30,7 +30,7 @@ WARNING!!! WARNING!!!
 // STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.)
+// (Note, Step 2 will be done in the SpeakHello.js file.) ЧЕ БЛЯТЬ ДЕЛАТЬ window то ли
 (function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
